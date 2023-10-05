819aea99dadb8d3d52046aabae01dd94d1076420
Recebov Ilkin <recebovilkin256@gmail.com>
2023-10-05 23:47:39 +0400

    Updated files

--- BB205 Week03/Task 2/Change_min_and_max.cs
+++ BB205 Week03/Task 2/Change_min_and_max.cs
@@ -8,40 +8,51 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 10, 4, 8, 2, -3, 4, 5 };
+            int[] numbers = { };
 
-            Console.Write("Our array's elements: ");
-            foreach (int i in numbers) Console.Write(i + " ");
-            MinAndMaxSwapIndex(numbers);
+            if (numbers.Length == 0)
+            {
+                MinAndMaxSwapIndex(numbers);
+            }
+            else
+            {
+                Console.Write("Our array's elements: ");
+                foreach (int i in numbers) Console.Write(i + " ");
+                MinAndMaxSwapIndex(numbers);
 
-            Console.Write("\n\nAfter the program our array: ");
-            foreach (int i in numbers) Console.Write(i + " ");
-            Console.WriteLine("\n\n\n");
+                Console.Write("\n\nAfter the program our array: ");
+                foreach (int i in numbers) Console.Write(i + " ");
+                Console.WriteLine("\n\n\n");
+            }
 
 
         }
         public static void MinAndMaxSwapIndex(params int[] arr)
         {
-            int min = arr[0], max = arr[0];
-            int minIndex = 0, maxIndex = 0, valueChanger = 0;
-
-            for (int i = 2; i < arr.Length; i++)
+            if (arr.Length == 0) Console.WriteLine("There is no element in array!");
+            else
             {
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                    minIndex = i;
-                }
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                    maxIndex = i;
-                }
-            }
+                int min = arr[0], max = arr[0];
+                int minIndex = 0, maxIndex = 0, valueChanger = 0;
 
-            valueChanger = arr[maxIndex];
-            arr[maxIndex] = arr[minIndex];
-            arr[minIndex] = valueChanger;
+                for (int i = 2; i < arr.Length; i++)
+                {
+                    if (arr[i] < min)
+                    {
+                        min = arr[i];
+                        minIndex = i;
+                    }
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                        maxIndex = i;
+                    }
+                }
+
+                valueChanger = arr[maxIndex];
+                arr[maxIndex] = arr[minIndex];
+                arr[minIndex] = valueChanger;
+            }
         }
     }
 }
