34c408d5fb762b160e2e0411d5961eb085c443c1
Recebov Ilkin <recebovilkin256@gmail.com>
2023-10-18 00:39:19 +0400

    New BB205 Week05 folder created!

---
+++ BB205 Week05/Models/ExtensionMethods.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods.Models
+{
+    internal static class ExtensionMethods
+    {
+        // Contain section
+        public static bool Contains(this string ourString, char ourChar)
+        {
+            bool checkChar = false;
+            for (int i = 0; i < ourString.Length; i++)
+            {
+                if (ourChar == ourString[i])
+                {
+                    checkChar = true;
+                    break;
+                }
+            }
+            return checkChar;
+        }
+        public static bool Contains(this string ourString, string findString)
+        {
+            int lengthOfFind = findString.Length;
+            bool checkFind = false;
+
+            if (ourString.Length < lengthOfFind)
+            {
+                checkFind = false;
+            }
+            else
+            {
+                int index = 0;
+                while (ourString.Length >= index + lengthOfFind)
+                {
+                    if (ourString.Substring(index, lengthOfFind) == findString)
+                    {
+                        checkFind = true;
+                        break;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+            }
+
+            return checkFind;
+        }
+
+        // Is power of two section
+        public static bool IsPowOfTwo(this int number)
+        {
+            int auxiliaryNumber = 0;
+
+            while (number > 0)
+            {
+                if (number % 2 != 0)
+                {
+                    auxiliaryNumber = number;
+                    break;
+                }
+                number /= 2;
+            }
+
+            if (auxiliaryNumber == 1) return true;
+            else return false;
+        }
+
+        // Is prime number section
+        public static bool IsPrime(this int number)
+        {
+            if(number < 0)
+            {
+                number = -1 * number;
+            }
+            bool result = true;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BB205 Week05/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExtentionMethods.Models;
+
+namespace ExtentionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======== Contain check ========");
+            Console.Write("Please, enter a text: ");
+            string userInput = Console.ReadLine();
+            Console.Write("Please, enter contain value(str || char): ");
+            string containInput = Console.ReadLine();
+            Console.WriteLine($"Answer: {userInput.Contains(containInput)}"); // ExtentionMethods.Contains(userInput, containInput);
+
+            Console.WriteLine("\n======== Is power of two check ========");
+            Console.Write("Please, enter a number: ");
+            int numberInput = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Answer: {ExtensionMethods.IsPowOfTwo(numberInput)}"); // numberInput.IsPowOfTwo();
+
+            Console.WriteLine("\n======== Is prime check ========");
+            Console.Write("Please, enter a number: ");
+            int primeInput = int.Parse( Console.ReadLine() );
+            Console.WriteLine($"Answer: {primeInput.IsPrime()}"); // ExtentionMethods.IsPrime(primeInput);
+
+        }
+    }
+}
