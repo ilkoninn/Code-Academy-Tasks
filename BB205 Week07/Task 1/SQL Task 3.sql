e40675d552a91b1b2c77fe7dd847f2dd03f87d0b
Ilkin <125400859+ilkoninn@users.noreply.github.com>
2023-11-02 23:18:52 +0400

    Update SQL Task 3.sql

--- BB205 Week07/Task 1/SQL Task 3.sql
+++ BB205 Week07/Task 1/SQL Task 3.sql
@@ -1,25 +1,26 @@
 CREATE DATABASE Users
+
 use Users
 
 CREATE TABLE Roles (
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY IDENTITY,
     Name VARCHAR(50)
 );
 
 CREATE TABLE Users (
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY IDENTITY,
     Username VARCHAR(50),
     Password VARCHAR(50),
     RoleID INT,
     FOREIGN KEY (RoleID) REFERENCES Roles(ID)
 );
 
-INSERT INTO Roles (ID, Name) VALUES (1, 'Admin');
-INSERT INTO Roles (ID, Name) VALUES (2, 'Moderator');
-INSERT INTO Roles (ID, Name) VALUES (3, 'User');
-INSERT INTO Users (ID, Username, Password, RoleID) VALUES (1, 'Ilkin', 'ilkin2004', 1);
-INSERT INTO Users (ID, Username, Password, RoleID) VALUES (2, 'Miraga', 'miraga2004', 2);
-INSERT INTO Users (ID, Username, Password, RoleID) VALUES (3, 'Tunay', 'tunay2004', 3);
+INSERT INTO Roles (Name) VALUES ('Admin');
+INSERT INTO Roles (Name) VALUES ('Moderator');
+INSERT INTO Roles (Name) VALUES ('User');
+INSERT INTO Users (Username, Password, RoleID) VALUES ('Ilkin', 'ilkin2004', 1);
+INSERT INTO Users (Username, Password, RoleID) VALUES ('Miraga', 'miraga2004', 2);
+INSERT INTO Users (Username, Password, RoleID) VALUES ('Tunay', 'tunay2004', 3);
 
 SELECT Users.Username, Roles.Name AS Role
 FROM Users
