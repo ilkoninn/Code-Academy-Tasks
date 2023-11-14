f8ef18eb69376e4c94cf2fc54235c8f33d7c303e
Ilkin <125400859+ilkoninn@users.noreply.github.com>
2023-11-14 15:09:28 +0400

    Update Program.cs

--- BB205 Week08/Task 1/Program.cs
+++ BB205 Week08/Task 1/Program.cs
@@ -29,26 +29,26 @@ namespace EntityFramework
                 Console.WriteLine($"{person.Id}|{person.Name}|{person.Surname}|{person.PhoneNumber}|{person.Address}|{person.City}");
             }
 
-            PersonServices services = new PersonServices();
+            CompanyServices companyServices = new CompanyServices();
 
             // Add Section
-            Person person1 = new Person("Miraga", "Eliyev", "Nizami rayonu", "Baku", "070 320 00 00");
-            services.Add(person1);
+            Company company1 = new Company("LimeFlix");
+            companyServices.Add(company1);
 
             // Delete Section
-            services.Delete(3);
+            companyServices.Delete(1);
 
             // Update Section
-            Person person3 = new Person(6, "Miraga", "Eliyev", "Nizami rayonu", "Baku", "070 320 00 00");
-            services.Update(person3);
+            Company company2 = new Company(1, "LabHome");
+            companyServices.Update(company2);
 
-            //Get All Section
-            var persons = services.GetAll();
+            // Get All Section
+            var companies = companyServices.GetAll();
+            Console.WriteLine("\tCompany table information\n");
 
-            Console.WriteLine("\tPerson table information\n");
-            foreach (var person in persons)
+            foreach (var company in companies)
             {
-                Console.WriteLine($"{person.Id}|{person.Name}|{person.Surname}|{person.PhoneNumber}|{person.Address}|{person.City}");
+                Console.WriteLine($"{company.Id}|{company.Title}");
             }
 
         }
